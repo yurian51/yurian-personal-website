5b4527286066305aa3adeec0eb92a2872ecef942
yurian51 <yurianarshad@gmail.com>
2026-09-09 11:48:29 +0300

    feat: add CMS foundation admin/logout.php

---
+++ admin/logout.php
@@ -0,0 +1,1 @@
+<?php declare(strict_types=1); require_once __DIR__.'/../config/bootstrap.php'; $_SESSION=[]; if(ini_get('session.use_cookies')){setcookie(session_name(),'',time()-42000,'/');} session_destroy(); header('Location:/admin/login.php'); exit;
