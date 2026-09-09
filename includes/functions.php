2af2ca874d36875cb8238b53f32611b7ab88474e
yurian51 <yurianarshad@gmail.com>
2026-09-09 11:45:51 +0300

    chore: add includes/functions.php

---
+++ includes/functions.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+function e(?string $value): string
+{
+    return htmlspecialchars($value ?? '', ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8');
+}
+
+function url(string $path = '/'): string
+{
+    return rtrim(getenv('APP_URL') ?: '', '/') . '/' . ltrim($path, '/');
+}
+
+function csrf_token(): string
+{
+    if (empty($_SESSION['_csrf'])) {
+        $_SESSION['_csrf'] = bin2hex(random_bytes(32));
+    }
+    return $_SESSION['_csrf'];
+}
+
+function verify_csrf(?string $token): bool
+{
+    return is_string($token) && hash_equals($_SESSION['_csrf'] ?? '', $token);
+}
