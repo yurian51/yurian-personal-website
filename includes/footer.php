86f7066a6de1c34a8537e19f242ad83e2a8babf9
yurian51 <yurianarshad@gmail.com>
2026-09-09 11:46:00 +0300

    chore: add includes/footer.php

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+  <div><strong>YURIAN.</strong><p>Building useful technology for the real world.</p></div>
+  <div>© <?= date('Y') ?> Yurian. All rights reserved.</div>
+</footer>
+<script src="/assets/js/app.js" defer></script>
+</body>
+</html>
