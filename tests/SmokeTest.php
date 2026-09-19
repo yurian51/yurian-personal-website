5a7bc3411c50bfea529125b044d7e27471b33012
yurian51 <yurianarshad@gmail.com>
2026-09-19 08:04:42 +0300

    test: cover seeded engineering experience

--- tests/SmokeTest.php
+++ tests/SmokeTest.php
@@ -27,6 +27,7 @@ assert(is_file($root . '/views/checkout.php'), 'Book checkout view must exist.')
 assert(is_file($root . '/database/migrations/004_bookstore.sql'), 'Bookstore migration must exist.');
 assert(is_file($root . '/database/migrations/005_personal_platform.sql'), 'Personal platform migration must exist.');
 assert(is_file($root . '/database/migrations/006_seed_jaslyn_details.sql'), 'Jaslyn dossier correction migration must exist.');
+assert(is_file($root . '/database/migrations/007_seed_experience.sql'), 'Experience seed migration must exist.');
 assert(is_file($root . '/views/engineering.php'), 'Engineering view must exist.');
 assert(is_file($root . '/views/case-studies.php'), 'Case study view must exist.');
 assert(is_file($root . '/views/cv.php'), 'Recruiter CV view must exist.');
